42ea951835b3121a882c0059413ddf9c615c8c7c
Julien Férard <julien_ferard@yahoo.fr>
2022-09-19 21:44:56 +0200

    Clean up

--- csv_transformer/transformation.py
+++ csv_transformer/transformation.py
@@ -401,7 +401,7 @@ class ColumnTransformationBuilder(BaseColumnTransformationBuilder):
 
     def build(self, col_id_str: str, col_visible: Optional[bool],
               col_type_str: str, col_filter_str: str, col_map_str: str,
-              col_rename_str: str, col_agg_str: str):
+              col_rename_str: str, col_agg_str: str) -> ColumnTransformation:
 
         col_rename = self._parse_col_rename(col_rename_str)
         if col_id_str is None:
@@ -449,7 +449,7 @@ class NewColumnTransformationBuilder(BaseColumnTransformationBuilder):
 
     def build(self, col_id_str: str, col_visible: Optional[bool],
               col_filter_str: Optional[str], col_formula_str: Optional[str],
-              col_name_str: Optional[str], col_agg_str: Optional[str]):
+              col_name_str: Optional[str], col_agg_str: Optional[str]) -> NewColumnTransformation:
         col_id = col_id_str
         col_visible = self._parse_col_visible(col_visible)
         col_filter = self._parse_col_filter(col_filter_str)
@@ -469,140 +469,6 @@ class NewColumnTransformationBuilder(BaseColumnTransformationBuilder):
 
     def _parse_col_rename(self, col_rename: str):
         self._rename = lambda _name: col_rename
-
-
-# class ColumnTransformationParser:
-#     _logger = logging.getLogger(__name__)
-#
-#     def __init__(self, transformation_builder: "TransformationBuilder",
-#                  default_column_transformation: DefaultColumnTransformation):
-#         self._transformation_builder = transformation_builder
-#         self._default_column_transformation = default_column_transformation
-#         self._col_visible = default_column_transformation.is_visible()
-#         self._type = id_func
-#         self._filter = true_func
-#         self._map = id_func
-#         self._agg = cast(Optional[ColAgg], None)
-#         self._rename = default_column_transformation.rename
-#         self._id = self._rename
-#
-#     def parse(self, json_col: JSONValue):
-#
-#         self._transformation_builder.column()
-#
-#     def _parse_col_type(self, col_type_str: str):
-#         try:
-#             self._type = self._transformation_builder.func_by_type(col_type_str)
-#         except KeyError:
-#             parser = self._transformation_builder.expression_parser()
-#             self._type = parser.parse(col_type_str)
-#
-#     def _parse_col_filter(self, col_filter_str: str):
-#         parser = self._transformation_builder.expression_parser()
-#         self._filter = parser.parse(col_filter_str)
-#
-#     def _parse_col_map(self, col_map_str: str):
-#         parser = self._transformation_builder.expression_parser()
-#         self._map = parser.parse(col_map_str)
-#
-#     def _parse_col_agg(self, col_agg_str: str):
-#         try:
-#             self._agg = self._transformation_builder.func_by_agg(col_agg_str)
-#         except KeyError:
-#             self._logger.exception("Agg error")
-#
-#     def _parse_col_rename(self, col_rename: str):
-#         self._rename = lambda _name: col_rename
-#
-#     def _parse_col_id(self, col_id_str: str):
-#         self._id = lambda _name: col_id_str
-
-
-# class NewColumnTransformationParser:
-#     _logger = logging.getLogger(__name__)
-#
-#     def __init__(self, parser_factory: "TransformationBuilder",
-#                  default_column_transformation: DefaultColumnTransformation):
-#         self._parser_factory = parser_factory
-#         self._default_column_transformation = default_column_transformation
-#         self._visible = default_column_transformation.is_visible()
-#         self._filter = true_func
-#         self._formula = empty_string_func
-#         self._agg = cast(Optional[ColAgg], None)
-#         self._rename = default_column_transformation.rename
-#         self._id = self._rename
-#
-#     def parse(self, json_col: JSONValue):
-#         try:
-#             self._visible = json_col["visible"]
-#         except KeyError:
-#             pass
-#
-#         try:
-#             col_filter_str = json_col["filter"]
-#         except KeyError:
-#             pass
-#         else:
-#             self._parse_col_filter(col_filter_str)
-#
-#         try:
-#             col_formula_str = json_col["formula_path"]
-#         except KeyError:
-#             pass
-#         else:
-#             self._parse_col_formula(col_formula_str)
-#
-#         try:
-#             col_rename_str = json_col["rename"]
-#         except KeyError:
-#             pass
-#         else:
-#             self._parse_col_rename(col_rename_str)
-#
-#         try:
-#             col_id_str = json_col["id"]
-#         except KeyError:
-#             pass
-#         else:
-#             self._parse_col_id(col_id_str)
-#
-#         try:
-#             col_agg_str = json_col["agg"]
-#         except KeyError:
-#             pass
-#         else:
-#             self._parse_col_agg(col_agg_str)
-#
-#         return NewColumnTransformation(
-#             self._visible, self._filter, self._formula, self._agg,
-#             self._rename, self._id)
-#
-#     def _parse_col_type(self, col_type_str: str):
-#         try:
-#             self._type = self._parser_factory.func_by_type(col_type_str)
-#         except KeyError:
-#             parser = self._parser_factory.expression_parser()
-#             self._type = parser.parse(col_type_str)
-#
-#     def _parse_col_filter(self, col_filter_str: str):
-#         parser = self._parser_factory.expression_parser()
-#         self._filter = parser.parse(col_filter_str)
-#
-#     def _parse_col_formula(self, col_formula_str: str):
-#         parser = self._parser_factory.expression_parser()
-#         self._map = parser.parse(col_formula_str)
-#
-#     def _parse_col_agg(self, col_agg_str: str):
-#         try:
-#             self._agg = self._parser_factory.func_by_agg(col_agg_str)
-#         except KeyError:
-#             self._logger.exception("Agg error")
-#
-#     def _parse_col_rename(self, col_rename: str):
-#         self._rename = lambda _name: col_rename
-#
-#     def _parse_col_id(self, col_id_str: str):
-#         self._id = lambda _name: col_id_str
 
 
 class TransformationBuilder:
@@ -649,7 +515,7 @@ class TransformationBuilder:
                                     self._prefix_unop_by_name,
                                     self._infix_unop_by_name)
 
-    def build(self):
+    def build(self) -> Transformation:
         return Transformation(self._entity_filter, self._agg_filter,
                               self._default_column_transformation,
                               self._col_transformation_by_name,
